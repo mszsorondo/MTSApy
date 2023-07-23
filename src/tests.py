9b16fbd84f22f07c09893e9dc98afdf282818d32
msorondo <msorondo@live.com.ar>
2023-07-23 20:20:22 -0300

    little advances with tests

--- src/tests.py
+++ src/tests.py
@@ -8,16 +8,23 @@ class CompositionGraphTests:
     def write_benchmark_feature_outputs(self):
         for problem in self.problems:
             for ins in self.instances:
+                f = open(f"test_outputs/{problem}_{ins[0]}_{ins[1]}", "w")
                 d = CompositionGraph(problem, ins[0], ins[1])
                 d.start_composition()
                 da = CompositionAnalyzer(d)
                 i = self.max_num_expansions
                 while (i and not d._javaEnv.isFinished()):
-                    raise NotImplementedError
                     d.expand(0)
-                    #[(da.compute_features(trans)) for trans in d.getFrontier()]
-                    #assert (d._expansion_order[-1] in [e[2]["action_with_features"] for e in d.edges(data=True)])
+                    last = d.getLastExpanded()
+                    f.write("LastExpanded: " + str(last.action.toString()) + " Resulting Frontier Size: " + str(len(d.getFrontier())) +"\n")
+                    frontier_features_image = ["\nSOURCE: " + str(trans.state.toString())+" -> TARGET: " + "None\n" + " \nFEATURES: \n" +
+                    str(da.compute_features(trans)) if trans.child is None else str(trans.child.toString())+ "FEATURES: \n" +
+                                                str(da.compute_features(trans)) for trans in d.getFrontier()]
+                    [f.write(im + "\n") for im in frontier_features_image]
+                    f.write("\n")
                     i-=1
+
+                f.close()
     def test_expansions_are_added(self):
         for problem in self.problems:
             for ins in self.instances:
@@ -35,7 +42,7 @@ class CompositionGraphTests:
 
 if __name__ == "__main__":
 
-    CompositionGraphTests().test_expansions_are_added()
+    CompositionGraphTests().write_benchmark_feature_outputs()
     """d = CompositionGraph("AT", 3, 3)
 
     d.start_composition()
