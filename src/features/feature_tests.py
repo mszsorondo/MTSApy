15a7d2c74f870bae2f819ff5d9dd7126015b89f7
Marco <msorondo@live.com.ar>
2023-09-29 12:40:19 -0300

    feature mapping to java sliced feature versions

--- src/features/feature_tests.py
+++ src/features/feature_tests.py
@@ -6,6 +6,19 @@ from features import *
 #REMEMBER this is only useful for testing features in the python implementation.
 # Once all tests pass, this test suite will no longer be that much useful
 # unless you do internal refactors to MTSA that impact on current implementation
+
+JAVA_METHOD_TO_PYTHON_METHOD = {
+    #TODO generalize testing functions for all features, put parameters
+    "alfComputeSlice" : EventLabel,
+    "slfComputeSlice" : StateLabel,
+    "isControllableComputeSlice" : Controllable,
+    "isMarkedComputeSlice": MarkedState,
+    "contextComputeSlice":CurrentPhase,
+    "childStatusComputeSlice" : ChildNodeState,
+    "uncontrollableComputeSlice" : UncontrollableNeighborhood,
+    "exploredComputeSlice" : ExploredStateChild,
+    "justExploredComputeSlice" : IsLastExpanded
+}
 def test_event_label_on_problem(problem,n,k, post_mortem_debug):
     cg = CompositionGraph(problem,n,k)
     cg.start_composition(no_tau=True)
